57b1f58000227b8b513151332f55c6f052ae41e3
Ebube Life <ebubeemeka19@gmail.com>
2023-02-18 17:06:51 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <>
   
     <section className="top-hero-section lg:h-screen lg:w-full  lg:grid lg:grid-cols-2 sm:no-grid  sm:grid-cols-1 gap-20 sm:gap-0 bg-gradient-to-t from-stone-200 to-white">
-    <div className="p-20 sm:pl-1 sm:pr-1 sm:screen lg:ml-20  sm:pt-2">
-      <h1 className=" sm:w-full lg:text-justify sm:ml-4 sm:text-center font-bold sm:text-4xl text-4xl mt-20 text-yellow-600 ">We summarise stuff</h1>
+    <div className="lg:p-20 sm:p-5 sm:pl-1 sm:pr-1 sm:h-full lg:ml-20  sm:pt-2">
+      <h1 className=" sm:w-full lg:text-justify sm:ml-42 sm:text-center font-bold sm:text-4xl text-4xl mt-20 text-yellow-600 ">We summarise stuff</h1>
       <p className="sm:text-center sm:ml-5 lg:text-justify   font-normal sm:text-2xl text-xl mt-3 text-stone-500">We create summaries from youtube videos, texts, books and audio files. with few clicks, you can get summaries in varying formats. Extract points, quotes or gain insights. {'\n'} Click the button below to get started with free credits.</p>
 
       <div className='w-48 bg-stone-800  sm: pl-20 lg:pl-5 '>
