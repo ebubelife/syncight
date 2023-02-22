5adee453fb9b37148dd15ebbd21669899fa052bb
Ebube Life <ebubeemeka19@gmail.com>
2023-02-22 22:29:07 +0100

    commit

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -15,11 +15,10 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                We Save Time By Summarising Videos, Text & Audio
+                Get straight to the point
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                Not everyone has the time to watch lengthy videos, listen to lengthy podcasts and read lengthy text repeating the same thing over and over. Convert video, text and audio content into short text summaries, get key points, quotes and actionable tips.
-                </p>
+                Simplify content consumption with powerful summarization technology. Customizable, accurate, and efficient for individuals and teams.    </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://nextjstemplates.com/templates/startup"
